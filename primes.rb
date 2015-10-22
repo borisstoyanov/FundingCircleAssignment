7c49fe3be732e00668074590a803ff10f7aad386
borisstoyanov <bss.stoyanov@gmail.com>
2015-10-22 23:04:40 +0300

    Adding README

--- primes.rb
+++ primes.rb
@@ -5,7 +5,7 @@ require './lib/PrimeMultiplier'
 options = {:position => nil}
 
 parser = OptionParser.new do|opts|
-  opts.banner = 'Usage: primes.rb [options]'
+  opts.banner = "Usage: primes.rb [options] \nRunning without an option displays the first 10 primes \n"
 
   opts.on('-p', '--position position', 'Displays prime number by given position') do |pos|
     options[:position] = pos
